389b699f70b681903879c213f375a04549f13c17
Your Full Name <your@ulster.ac.uk>
2026-06-28 21:29:05 +0100

    Prepare PLEROMA for deployment

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const http = require("http");
 const { Server } = require("socket.io");
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 const db = new sqlite3.Database("./database.db");
 
 const server = http.createServer(app);
